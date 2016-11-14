a794c491069d63d9ac2d79a3c677adade196712a
Martin Kierkegaard <mark@easj.dk>
2016-11-14 22:45:09 +0100

    added Worker, Manager, Director

---
+++ Sandbox/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Manager : Employee 
+    {
+        private int monthlyBonus;
+        private int hoursWorked;
+
+        public Manager(String name, int salaryPerMonth, int monthlyBonus)
+            : base(name, salaryPerMonth)
+        {
+            this.monthlyBonus = monthlyBonus;
+            hoursWorked = 0;
+        }
+
+        public void setHoursWorked(int hoursWorked)
+        {
+            this.hoursWorked = hoursWorked;
+        }
+
+        public override int GetSalaryPerMonth()
+        {
+            int totalSalary = base.GetSalaryPerMonth();
+
+            if (hoursWorked > 180)
+            {
+                totalSalary = totalSalary + monthlyBonus;
+            }
+
+            return totalSalary;
+        }
+    }
+}
---
+++ Sandbox/Worker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+        private String skill;
+
+        public Worker(String name, int salaryPerMonth, String skill)
+            : base(name, salaryPerMonth)
+        {
+            this.skill = skill;
+        }
+
+        public String GetSkill()
+        {
+            return skill;
+        }
+
+        public void SetSkill(String skill)
+        {
+            this.skill = skill;
+        }
+    }
+}
